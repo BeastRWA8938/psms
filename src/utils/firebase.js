f20c0fe7f0fcfab15f1d076a8681b3ae613056df
BeastRWA8938 <57098526+BeastRWA8938@users.noreply.github.com>
2024-03-31 19:12:33 +0530

    Update firebase.js
    removed keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyQV9pv70C6bZjeBNZapEn7CMPPq9Klyc",
-  authDomain: "parkingspacems-105bf.firebaseapp.com",
-  databaseURL: "https://parkingspacems-105bf-default-rtdb.firebaseio.com",
-  projectId: "parkingspacems-105bf",
-  storageBucket: "parkingspacems-105bf.appspot.com",
-  messagingSenderId: "828481037651",
-  appId: "1:828481037651:web:a187c639dee96012262da6",
-  measurementId: "G-6YWQQE6CT5"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
